b8c04d7d628c7b9ff438ca64a31324ade59c3236
unknown <kzyjjkuo@gmail.com>
2022-06-13 12:28:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import DressingRoom from './DressingRoom/DressingRoom';
+import Cover from './test/Covertest';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Covertest from './test/Covertest';
+
+function App() {
+  return (
+    <div className="App">
+      <Covertest/>
+    </div>
+  );
+}
+
+export default App;
